47d26e0937c06115fb4ae39ae302f2f343ea1662
Caio Lima <caio.lima1991@icloud.com>
2021-09-05 22:03:02 -0300

    Primeiro Commit

---
+++ product-catalog/src/main/java/org/digitalinnovation/experts/productcatalog/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package org.digitalinnovation.experts.productcatalog.repository;
+
+import org.digitalinnovation.experts.productcatalog.model.Product;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductRepository extends CrudRepository<Product, Integer> {
+}
